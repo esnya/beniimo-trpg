6efd69a5f57b7caacf962b3c8d29f2bbf40f30da
ukatama <dev.ukatama@gmail.com>
2016-01-01 03:12:19 +0900

    Change MyAuthAuthenticate::getUser()

--- Controller/Component/Auth/MyAuthAuthenticate.php
+++ Controller/Component/Auth/MyAuthAuthenticate.php
@@ -1,17 +1,23 @@
 <?php
-	App::uses('BaseAuthenticate', 'Controller/Component/Auth');
+    App::uses('BaseAuthenticate', 'Controller/Component/Auth');
+    
+    class MyAuthAuthenticate extends BaseAuthenticate {
+        public function authenticate(CakeRequest $request, CakeResponse $response) {
+            return true;
+        }
 
-	class MyAuthAuthenticate extends BaseAuthenticate {
-		public function authenticate(CakeRequest $request, CakeResponse $response) {
-			return true;
-		}
-		public function getUser(CakeRequest $request) {
-			$auth = Hash::get($_SERVER, 'PHP_AUTH_DIGEST');
-			if ($auth) {
-				$match = preg_match('/username="([^"]+)"/', $auth, $matches);
-
-				if (count($matches) == 2) return ['id' => $matches[1]];
-			} 
-			return false;
-		}
-	}
+        public function getUser(CakeRequest $request) {
+            $key = Hash::get($_ENV, 'USER_KEY', 'REMOTE_USER');
+            if (!$key) {
+                $key = 'REMOTE_USER';
+            }
+    
+            $user = env($key);
+    
+            if ($user) {
+                return [ 'id' => $user ];
+            } 
+    
+            return false;
+        }
+    }
